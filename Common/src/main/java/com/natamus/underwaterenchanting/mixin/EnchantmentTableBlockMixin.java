7195deda69c9f08fe69da5f260e5b3d338522984
Rick South <rick@natamus.com>
2023-12-22 18:09:58 +0100

    (v2.7) Moved mixin files to the Common source folder to make future development easier.

--- Common/src/main/java/com/natamus/underwaterenchanting/mixin/EnchantmentTableBlockMixin.java
+++ Common/src/main/java/com/natamus/underwaterenchanting/mixin/EnchantmentTableBlockMixin.java
@@ -1,4 +1,4 @@
-package com.natamus.underwaterenchanting.forge.mixin;
+package com.natamus.underwaterenchanting.mixin;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.Level;
