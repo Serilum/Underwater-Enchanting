3b0e608676d02715c1a2c48efeb1ce825fb6aa98
Rick South <rick@natamus.com>
2024-10-24 17:56:49 +0200

    Underwater Enchanting source code for 1.21.2.

---
+++ Common/src/main/java/com/natamus/underwaterenchanting/util/Reference.java
@@ -0,0 +1,8 @@
+package com.natamus.underwaterenchanting.util;
+
+public class Reference {
+	public static final String MOD_ID = "underwaterenchanting";
+	public static final String NAME = "Underwater Enchanting";
+	public static final String VERSION = "2.9";
+	public static final String ACCEPTED_VERSIONS = "[1.21.2]";
+}
